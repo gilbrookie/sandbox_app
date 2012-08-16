570a72a0f39a67a42108c7ec6dc400dde27f0e1d
James Gilbrook <james@gilbrook.ca>
2012-08-15 22:21:51 -0400

    Adding support for serving files.
    Includes two new views and a file_store directory to the tree.

--- sandbox_web/views/__init__.py
+++ sandbox_web/views/__init__.py
@@ -1,13 +1,20 @@
 
+import os
+from pprint import pprint as pp
+
 import api
 from sandbox_web import app
-from flask import g, session, redirect, render_template, abort, request, flash, url_for
-from pprint import pprint as pp
+
+from flask import g, session, redirect, render_template, abort, \
+                    request, flash, url_for, send_from_directory
+
 
 @app.route("/")
 def home():
+    """This function peforms the home page view"""
     pp(api.blog.get_blog())
-    return render_template("show_entries.html", entries=api.blog.get_blog())
+    return render_template("show_entries.html", 
+                            entries=api.blog.get_blog())
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -17,4 +24,18 @@ def login():
 def logout():
     pass
 
+@app.route("/file/<path:filename>")
+def get_file(filename):
+    """This function serves file from the file_store directory;
+    Returns the file provided by the path:filename argument"""
 
+    print app.config
+    return send_from_directory(os.getcwd()+"/file_store/", 
+                               filename, as_attachment=True)
+
+@app.route("/file/")
+@app.route("/files/")
+def list_files():
+    """Lists all files available in the filestore"""
+    return render_template("files.html", 
+                            file_list=os.listdir(os.getcwd()+"/file_store/")) 
